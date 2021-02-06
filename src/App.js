2af8611d778762b1a21e326979592157051fdaca
David Galvis <ivan.galvis@project44.com>
2021-02-05 23:54:14 -0500

    section 11. adding pages into the navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from "react-router-dom";
 
 import Blog from './containers/Blog/Blog';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Blog />
-      </div>
+        //Here we encapsulate all the components that need from the routing to be shown
+        <BrowserRouter>
+          <div className="App">
+            <Blog />
+          </div>
+        </BrowserRouter>
     );
   }
 }
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,8 +3,8 @@ import React, {Component} from 'react';
 import axios from '../../axios'
 
 import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import FullPost from '../FullPost/FullPost';
+import NewPost from '../NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -61,6 +61,14 @@ class Blog extends Component {
 
         return (
             <div>
+                <header className="Blog">
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/new-post">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
                 <section className="Posts">
                     {posts}
                 </section>

6a8cec1752d7e05f8aaa7ed08ce952890eeaf373
David Galvis <ivan.galvis@project44.com>
2021-02-08 21:42:23 -0500

    section 11. Preparing the Project For Routing - refactoring Posts component

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,63 +2,14 @@ import React, {Component} from 'react';
 //This is the file where we have defined the new instance with the 'custom' values
 import axios from '../../axios'
 
-import Post from '../../components/Post/Post';
-import FullPost from '../FullPost/FullPost';
-import NewPost from '../NewPost/NewPost';
+import Posts from '../Posts/Posts';
+
 import './Blog.css';
 
 class Blog extends Component {
 
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
-
-    componentDidMount() {
-        //since this happens asynchronously, we treat is at a promise when we call the url and wait until it gets the request, and then we execute something
-        //but it won't happen right away
-        //with the baseURL set there is no need of it here
-        axios.get('/posts')
-            .then(response => {
-                console.log(response);
-                const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Max'
-                    }
-                });
-                this.setState({posts: updatedPosts})
-            })
-            .catch(error => {
-                console.log(error);
-                this.setState({error: true})
-            })
-    }
-
-    postSelectHandler = (id) => {
-        this.setState({selectedPostId: id})
-    }
 
     render() {
-
-        let posts;
-
-        if (this.state.error) {
-            posts = <p>Something went wrong</p>
-        } else {
-            posts = this.state.posts.map(post => {
-                //Never forget to set the key
-                return <Post
-                    key={post.id}
-                    title={post.title}
-                    author={post.author}
-                    clicked={() => this.postSelectHandler(post.id)}
-                />;
-            })
-        }
-
         return (
             <div>
                 <header className="Blog">
@@ -69,15 +20,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId}/>
-                </section>
-                <section>
-                    <NewPost/>
-                </section>
+                <Posts/>
             </div>
         );
     }
---
+++ src/containers/Posts/Posts.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import axios from "../../axios";
+import Post from "../../components/Post/Post";
+
+class Posts extends Component {
+
+    state = {
+        posts: [],
+    }
+
+    componentDidMount() {
+        //since this happens asynchronously, we treat is at a promise when we call the url and wait until it gets the request, and then we execute something
+        //but it won't happen right away
+        //with the baseURL set there is no need of it here
+        axios.get('/posts')
+            .then(response => {
+                console.log(response);
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                });
+                this.setState({posts: updatedPosts})
+            })
+            .catch(error => {
+                console.log(error);
+                // this.setState({error: true})
+            })
+    }
+
+    postSelectHandler = (id) => {
+        this.setState({selectedPostId: id})
+    }
+
+    render() {
+        let posts;
+
+        if (this.state.error) {
+            posts = <p>Something went wrong</p>
+        } else {
+            posts = this.state.posts.map(post => {
+                //Never forget to set the key
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectHandler(post.id)}
+                />;
+            })
+        }
+
+        return (
+            <section className="Posts">
+                {posts}
+            </section>
+        )
+
+    }
+}
+
+export default Posts;

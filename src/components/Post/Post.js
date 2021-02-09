1f92700b257a2bda12210b16d78452253bf0ef70
David Galvis <ivan.galvis@project44.com>
2021-02-08 22:57:31 -0500

    section 11. The withRouter HOC & Route Props

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import {withRouter} from "react-router-dom";
 
 import './Post.css';
 
-const post = (props) => (
-    <article className="Post" onClick={props.clicked}>
+const post = (props) => {
+    console.log(this.props);
+    return (<article className="Post" onClick={props.clicked}>
         <h1>{props.title}</h1>
         <div className="Info">
             <div className="Author">{props.author}</div>
         </div>
-    </article>
-);
+    </article>)
+};
 
 export default post;
+//we may want to get access to the nearest Route props that are being passed, to the parent component,
+// from an even parent-greater component, so to do so we can chain properties as the normal component passing properties from component A to C,
+// or using a HOC withRouter, wrapping the component where we need to see the properties with that HOC, like: export default withRouter(componentName);
+// export default withRouter(post);
